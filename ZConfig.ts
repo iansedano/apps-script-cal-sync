d417470efd365fe754e5d4e2e05e3d14a31c4979
Ian <iansedano@gmail.com>
2022-05-28 16:31:34 +0200

    removing SheetDb

--- ZConfig.ts
+++ ZConfig.ts
@@ -1,20 +1,21 @@
 interface ConfigObject {
   EVENT_DATA_SSID: string;
   TEST_EVENT_DATA_SSID: string;
+  TEST_EVENT_SHEET_NAME: string;
   TIME_RANGE: CalTyp.TimeRange;
-  STORAGE: _Table;
 }
 
 const Config: ConfigObject = {
   EVENT_DATA_SSID: "1_XVFb2erZT9EyevCnqRr0UH2kpNFoMl_CO726UivXCk",
   TEST_EVENT_DATA_SSID: "1hkyW1CybhzjZWFQrvkiKtkJSxfG8OzYJqb4hAhSN2sE",
+  TEST_EVENT_SHEET_NAME: "cal-sync",
   TIME_RANGE: DateUtil.getTimeRange(-1, 7),
 };
 
 const STORAGE = new Table(
-  SpreadsheetApp.openById(
-    "1hkyW1CybhzjZWFQrvkiKtkJSxfG8OzYJqb4hAhSN2sE"
-  ).getSheetByName("cal-sync")
+  SpreadsheetApp.openById(Config.TEST_EVENT_DATA_SSID).getSheetByName(
+    Config.TEST_EVENT_SHEET_NAME
+  )
 );
 
 // namespace Config {
