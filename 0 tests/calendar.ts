4b034eac52b0ac807b0c7e4778a75ebcd1cccb70
Ian <iansedano@gmail.com>
2022-06-05 16:21:17 +0200

    creating events and linking them

--- 0 tests/calendar.ts
+++ 0 tests/calendar.ts
@@ -1,5 +1,16 @@
-function test_initial_sync() {
-  initialSyncCal("ian@realpython.com");
+function test_initial_sync_2() {
+  initialSyncCal(
+    CalendarApp.getCalendarById("ian@realpython.com"),
+    Config.TIME_RANGE,
+    CalendarApp.getCalendarById(
+      "9cmp5khc3e1mdbm4quvrfbk60g@group.calendar.google.com"
+    )
+  );
   Utilities.sleep(1000);
-  STORAGE.clearAllEntries();
 }
+
+// function test_initial_sync() {
+//   initialSyncCal("ian@realpython.com");
+//   Utilities.sleep(1000);
+//   STORAGE.clearAllEntries();
+// }
--- src/Cal.ts
+++ src/Cal.ts
@@ -17,7 +17,7 @@ function extractKeyEventData(
 function getEventListFromCal(
   calId: string,
   timeRange: CalTyp.TimeRange
-): Array<CalTyp.EventEntry> {
+): CalTyp.EventEntry[] {
   return CalendarApp.getCalendarById(calId)
     .getEvents(...timeRange)
     .map(extractKeyEventData)
@@ -27,55 +27,91 @@ function getEventListFromCal(
 }
 
 function initialSyncCal(
-  calId: string,
-  timeRange: CalTyp.TimeRange = Config.TIME_RANGE
+  sourceCal: GoogleAppsScript.Calendar.Calendar,
+  timeRange: CalTyp.TimeRange = Config.TIME_RANGE,
+  targetCal: GoogleAppsScript.Calendar.Calendar
 ): void {
-  const eventList = getEventListFromCal(calId, timeRange);
-  console.log({ calId });
-  const storedEvents = STORAGE.getEntries({ calId });
-  console.log({ storedEvents });
+  const eventList = getEventListFromCal(sourceCal.getId(), timeRange);
+  const storedEvents = STORAGE.getEntries({ calId: sourceCal.getId() });
   if (storedEvents.length !== 0) {
-    throw `Sheet ${calId} has data, please clear before making initial sync`;
+    throw `Sheet ${sourceCal.getId()} has data, please clear before making initial sync`;
   }
+  const newEventList = createLinkedEvents(eventList, targetCal);
 
-  STORAGE.addEntries(eventList);
+  STORAGE.addEntries(newEventList);
+}
+
+function createLinkedEvents(
+  events: CalTyp.EventEntry[],
+  targetCal: GoogleAppsScript.Calendar.Calendar
+): CalTyp.EventEntry[] {
+  return events.map((event) => {
+    const id = targetCal
+      .createEvent(event.title, event.start, event.end)
+      .getId();
+    event.linkedCalId = targetCal.getId();
+    event.linkedEventId = id;
+    return event;
+  });
 }
 
 function checkForUpdatedEvents(
-  originEvents: Array<CalTyp.EventEntry>,
-  savedOriginEvents: Array<CalTyp.EventEntry>
+  sourceCal: GoogleAppsScript.Calendar.Calendar,
+  targetCal: GoogleAppsScript.Calendar.Calendar
 ): CalTyp.UpdateList {
   const updatedEvents = [];
   const newEvents = [];
   const deletedEvents = [];
 
+  const savedEvents = STORAGE.getEntries({
+    calId: sourceCal.getId(),
+    linkedCalId: targetCal.getId(),
+  });
+
+  savedEvents.forEach((event: CalTyp.EventEntry) => {
+    const actualEvent = CalendarApp.getEventById(event.eventId);
+
+    if (!actualEvent) {
+      deletedEvents.push(event.id);
+      return;
+    }
+
+    const actualEventData = extractKeyEventData(actualEvent);
+
+    if (
+      !Object.entries(actualEvent).every(([key, value]) => event[key] == value)
+    ) {
+      updatedEvents.push(event.id);
+    }
+  });
+
   // Check for modified and new events
-  originEvents.forEach((event: CalTyp.EventEntry) => {
-    const savedEvent = savedOriginEvents.find(
-      (savedEvent: CalTyp.EventEntry) => {
-        return savedEvent.eventId == event.eventId;
-      }
-    );
+  // savedEvents.forEach((event: CalTyp.EventEntry) => {
+  //   const savedEvent = savedOriginEvents.find(
+  //     (savedEvent: CalTyp.EventEntry) => {
+  //       return savedEvent.eventId == event.eventId;
+  //     }
+  //   );
 
-    if (savedEvent) {
-      for (const [key, value] of Object.entries(savedEvent)) {
-        if (event[key] !== value) {
-          updatedEvents.push(savedEvent);
-          break;
-        }
-      }
-    } else newEvents.push(savedEvent);
-  });
+  //   if (savedEvent) {
+  //     for (const [key, value] of Object.entries(savedEvent)) {
+  //       if (event[key] !== value) {
+  //         updatedEvents.push(savedEvent);
+  //         break;
+  //       }
+  //     }
+  //   } else newEvents.push(savedEvent);
+  // });
 
-  // Check for deleted events
-  savedOriginEvents.forEach((event: CalTyp.EventEntry) => {
-    const originEvent = originEvents.find((originEvent: CalTyp.EventEntry) => {
-      return originEvent.eventId == event.eventId;
-    });
-    if (!originEvent) deletedEvents.push(event);
-  });
+  // // Check for deleted events
+  // savedOriginEvents.forEach((event: CalTyp.EventEntry) => {
+  //   const originEvent = originEvents.find((originEvent: CalTyp.EventEntry) => {
+  //     return originEvent.eventId == event.eventId;
+  //   });
+  //   if (!originEvent) deletedEvents.push(event);
+  // });
 
-  return { newEvents, updatedEvents, deletedEvents };
+  // return { newEvents, updatedEvents, deletedEvents };
 }
 
 function createNewEvents(
